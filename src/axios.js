da61214be0a3b232fa0e3d76a73e3055474e2ef6
Nayemul <nayemulhasan2016@gmail.com>
2020-11-18 21:20:03 +0600

    initial commit

---
+++ src/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+// base url to make requests to the movie daabase
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+});
+// instance.get('/');
+export default instance;
