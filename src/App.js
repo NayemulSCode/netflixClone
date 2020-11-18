710f5f7b75840b13dcce9e17ca03b81a2693a5a3
Nayemul <nayemulhasan2016@gmail.com>
2020-11-18 23:29:45 +0600

    add Banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import Banner from './Banner';
 import requests from './request';
 import Row from './Row';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello REact</h1>
-      <Row title = "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+    <div className="app">
+      <Banner />
+      <Row 
+        title = "NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargerRow={true}
+      />
       <Row title = "Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title = "Top Ratted" fetchUrl={requests.fetchTopRated}/>
+      <Row title = "Romantic Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import './Row.css'
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargerRow}) {
     const [movies, setMovies] = useState([]);
     //a snippet of code which runs based on a specific conditon
     useEffect(() =>{
@@ -26,8 +26,9 @@ function Row({title, fetchUrl}) {
                 {movies.map(movie => (
                     <img 
                      key={movie.id}
-                     className="row__poster"
-                     src={`${base_url}${movie.poster_path}`}alt={movie.name} 
+                     className={`row__poster ${isLargerRow && "row__posterLarger"}`}
+                     src={`${base_url}${isLargerRow ? movie.poster_path : movie.backdrop_path}`}
+                     alt={movie.name} 
                     />
                 ))}
             </div>
